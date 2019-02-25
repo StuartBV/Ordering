3c0900d06a6572a664f7af9e6db2fe8bffb1aaca
StuartBV <stuart@bevalued.co.uk>
2019-02-25 12:20:34 +0000

    Static data

---
+++ db/Ordering/Data/dbo.Sender_Email_AdditionalRecipients_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO [dbo].[Sender_Email_AdditionalRecipients] ([RecipientType], [EmailAddress], [ServerType]) VALUES (2, 'dev-system@bevalued.co.uk', 'Dev')
+INSERT INTO [dbo].[Sender_Email_AdditionalRecipients] ([RecipientType], [EmailAddress], [ServerType]) VALUES (2, 'dfpurchaseorders@powerplaydirect.co.uk', 'Live')
+INSERT INTO [dbo].[Sender_Email_AdditionalRecipients] ([RecipientType], [EmailAddress], [ServerType]) VALUES (3, 'dave.morrow@bevalued.co.uk', 'Dev')
+INSERT INTO [dbo].[Sender_Email_AdditionalRecipients] ([RecipientType], [EmailAddress], [ServerType]) VALUES (3, 'mark.harrison@bevalued.co.uk', 'Dev')
