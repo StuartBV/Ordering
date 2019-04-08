d28d1d039938ca4db180bc53dc6f282795a6920a
StuartBV <stuart@bevalued.co.uk>
2019-04-08 15:16:53 +0100

    Reorder

--- db/Ordering/Tables/dbo.OrderSenders_Config.sql
+++ db/Ordering/Tables/dbo.OrderSenders_Config.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [dbo].[OrderSenders_Config]
 (
 [SupplierId] [int] NOT NULL,
-[OrderSenderId] [int] NOT NULL,
 [ProductFulfilmentType] [smallint] NOT NULL,
+[SourceType] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [D_OrderSenders_Config_SourceType] DEFAULT ('*'),
+[OrderSenderId] [int] NOT NULL,
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[CreateDate] [datetime] NOT NULL,
-[CreatedBy] [dbo].[UserID] NOT NULL,
+[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_OrderSenders_Config_CreateDate] DEFAULT (getdate()),
+[CreatedBy] [dbo].[UserID] NOT NULL CONSTRAINT [DF_OrderSenders_Config_CreatedBy] DEFAULT ('sys'),
 [AlteredDate] [datetime] NULL,
-[AlteredBy] [dbo].[UserID] NULL,
-[SourceType] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [D_OrderSenders_Config_SourceType] DEFAULT ('*')
+[AlteredBy] [dbo].[UserID] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[OrderSenders_Config] ADD CONSTRAINT [PK_OrderSenders_Config] PRIMARY KEY CLUSTERED  ([SupplierId], [ProductFulfilmentType], [SourceType]) WITH (FILLFACTOR=100) ON [PRIMARY]
--- db/Ordering/Tables/dbo.Ordering_Claims.sql
+++ db/Ordering/Tables/dbo.Ordering_Claims.sql
@@ -5,11 +5,12 @@ CREATE TABLE [dbo].[Ordering_Claims]
 [InsuranceClaimNo] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Excess] [smallmoney] NOT NULL,
 [IncidentDate] [smalldatetime] NULL,
+[ExcessCollectedByBV] [bit] NULL,
 [CreateDate] [datetime] NOT NULL CONSTRAINT [DF_Ordering_Claim_CreateDate] DEFAULT (getdate()),
-[CreatedBy] [dbo].[UserID] NOT NULL,
+[CreatedBy] [dbo].[UserID] NOT NULL CONSTRAINT [DF_Ordering_Claims_CreatedBy] DEFAULT ('sys'),
 [AlteredDate] [datetime] NULL,
 [AlteredBy] [dbo].[UserID] NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Ordering_Claims] ADD CONSTRAINT [PK_Ordering_Claims] PRIMARY KEY CLUSTERED  ([DeliveryId]) ON [PRIMARY]
+ALTER TABLE [dbo].[Ordering_Claims] ADD CONSTRAINT [PK_Ordering_Claims] PRIMARY KEY CLUSTERED  ([DeliveryId]) WITH (FILLFACTOR=95) ON [PRIMARY]
 GO
